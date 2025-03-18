1c700e4b0ca48ecc9a4fe429128ffbcc1e344740
Oij13 <oij13@icloud.com>
2025-03-17 21:33:12 -0400

    Small number implementation

--- src/main/java/edu/bsu/cs/Converter.java
+++ src/main/java/edu/bsu/cs/Converter.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 
 public class Converter {
     public String convert(int number){
-        return "I";
+        StringBuilder result = new StringBuilder();
 
+        int [] numbers = {10,9,5,4,1};
+        String [] roman = {"X","IX","V","IV","I"};
+
+        for (int i = 0; i < numbers.length; i++){
+            while (number >= numbers[i]){
+                result.append(roman[i]);
+                number -= numbers[i];
+            }
+        }
+
+        return result.toString();
     }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
--- src/test/java/edu/bsu/cs/ConverterTest.java
+++ src/test/java/edu/bsu/cs/ConverterTest.java
@@ -9,6 +9,41 @@ public class ConverterTest {
     public void convert1toiTest(){
         Converter converter = new Converter();
         Assertions.assertEquals("I",converter.convert(1));
-
     }
+    @Test
+    public void convert3toiiiTest(){
+        Converter converter = new Converter();
+        Assertions.assertEquals("III",converter.convert(3));
+    }
+    @Test
+    public void convert5toVTest(){
+        Converter converter = new Converter();
+        Assertions.assertEquals("V",converter.convert(5));
+    }
+    @Test
+    public void convert10toXTest(){
+        Converter converter = new Converter();
+        Assertions.assertEquals("X",converter.convert(10));
+    }
+    @Test
+    public void convert4toIVTest(){
+        Converter converter = new Converter();
+        Assertions.assertEquals("IV",converter.convert(4));
+    }
+    @Test
+    public void convert100tocTest(){
+        Converter converter = new Converter();
+        Assertions.assertEquals("C",converter.convert(100));
+    }
+    @Test
+    public void convert500toiTest(){
+        Converter converter = new Converter();
+        Assertions.assertEquals("D",converter.convert(500));
+    }
+    @Test
+    public void convert1000tomTest(){
+        Converter converter = new Converter();
+        Assertions.assertEquals("M",converter.convert(1000));
+    }
+
 }
